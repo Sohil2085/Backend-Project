5a3e9276823a12b81f7f89942eb77c0b1e4fdb76
Sohil2085 <sohilkareliya9695@gmail.com>
2025-07-26 18:01:30 +0530

    `Added cookie-parser and cors dependencies, updated package-lock.json and package.json, and modified app.js and index.js to use these dependencies.`

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
 
+
 dotenv.config({
     path : './.env'
 })
 
 connectDB()
-
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000,() => {
+        console.log(`server is running on port ${process.env.PORT || 8000}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed"+err);
+})
 
 
 
@@ -19,7 +26,7 @@ connectDB()
 
 // import express from "express";
 // const app = express();
-
+//with iffie
 // ( async () => {
 //     try {
 //         await mongoose.connect(`${process.env.MONGODB_URI}`)
